71cb77de3e123de98fdd88d1c654b39633a7a57f
yshalsager <ysh-alsager@hotmail.com>
2022-08-05 22:46:27 +0200

    fix(rtl): render text in correct direction based on OS
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- wit_transcriber/gui/main_window.py
+++ wit_transcriber/gui/main_window.py
@@ -3,16 +3,16 @@ import tkinter as tk
 import tkinter.font as tkFont
 from asyncio import create_task, get_event_loop, sleep
 from pathlib import Path
+from platform import system
 from tkinter import StringVar, filedialog, messagebox
 from webbrowser import open_new_tab
-
-from awesometkinter.bidirender import render_text
 
 from wit_transcriber import PARENT_DIR
 from wit_transcriber.api_client.client import WitAiAPI
 from wit_transcriber.gui import constants
 from wit_transcriber.gui.preferences import PreferencesManager
 from wit_transcriber.gui.settings import SettingWindow
+from wit_transcriber.gui.utils import _text
 
 
 class IORedirector:
@@ -30,7 +30,7 @@ class StdoutRedirector(IORedirector):
         super().__init__(text_area)
 
     def write(self, string: str) -> None:
-        self.text_area.insert("end", render_text(string))
+        self.text_area.insert("end", _text(system(), string))
         self.text_area.see("end")
 
     def flush(self) -> None:
@@ -45,6 +45,7 @@ https://www.loekvandenouweland.com/content/python-asyncio-and-tkinter.html
 
 class App:
     def __init__(self) -> None:
+        self._platform = system()
         self.parent: tk.Tk = tk.Tk()
         self.parent.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.parent.title("أداة التفريغ الصوتي")
@@ -58,7 +59,7 @@ class App:
 
         self.menu = tk.Menu(self.parent)
         self.label = tk.Label(
-            self.parent, text=render_text("wit.ai أداة للتفريغ الصوتي باستخدام")
+            self.parent, text=self.render_text("wit.ai أداة للتفريغ الصوتي باستخدام")
         )
         self.input_entry = tk.Entry(self.parent, textvariable=self.input_path, width=60)
         self.output_entry = tk.Entry(
@@ -66,7 +67,7 @@ class App:
         )
         self.startTranscribe = tk.Button(
             self.parent,
-            text=render_text(constants.SUBMIT_BUTTON),
+            text=self.render_text(constants.SUBMIT_BUTTON),
             command=lambda: create_task(self.get_transcribe()),
         )
         self.scrollbar = tk.Scrollbar(self.parent, orient=tk.VERTICAL)
@@ -86,28 +87,30 @@ class App:
     def init_ui(self) -> None:
         file_menu = tk.Menu(self.menu, tearoff=0)
         file_menu.add_command(
-            label=render_text(constants.MENU_BAR_FILE_NEW),
+            label=self.render_text(constants.MENU_BAR_FILE_NEW),
             command=self.ask_for_input_path,
         )
         file_menu.add_command(
-            label=render_text(constants.MENU_BAR_FILE_SETTINGS), command=self.open_win
+            label=self.render_text(constants.MENU_BAR_FILE_SETTINGS),
+            command=self.open_win,
         )
         file_menu.add_separator()
         file_menu.add_command(
-            label=render_text(constants.MENU_BAR_FILE_EXIT), command=self.on_closing
+            label=self.render_text(constants.MENU_BAR_FILE_EXIT),
+            command=self.on_closing,
         )
         help_menu = tk.Menu(self.menu, tearoff=0)
         help_menu.add_command(
-            label=render_text(constants.MENU_BAR_ABOUT),
+            label=self.render_text(constants.MENU_BAR_ABOUT),
             command=lambda: open_new_tab(
                 "https://github.com/yshalsager/wit_transcriber"
             ),
         )
         self.menu.add_cascade(
-            label=render_text(constants.MENU_BAR_FILE), menu=file_menu
+            label=self.render_text(constants.MENU_BAR_FILE), menu=file_menu
         )
         self.menu.add_cascade(
-            label=render_text(constants.MENU_BAR_HELP), menu=help_menu
+            label=self.render_text(constants.MENU_BAR_HELP), menu=help_menu
         )
         self.parent.config(menu=self.menu)
 
@@ -117,13 +120,13 @@ class App:
 
         tk.Button(
             self.parent,
-            text=render_text(constants.INPUT_BUTTON_TITLE),
+            text=self.render_text(constants.INPUT_BUTTON_TITLE),
             command=self.ask_for_input_path,
         ).grid(row=1, column=1, pady=10, padx=10)
 
         tk.Button(
             self.parent,
-            text=render_text(constants.OUTPUT_BUTTON_TITLE),
+            text=self.render_text(constants.OUTPUT_BUTTON_TITLE),
             command=self.ask_for_output_path,
         ).grid(row=3, column=1, pady=10, padx=10)
 
@@ -140,7 +143,7 @@ class App:
             self.parent,
             variable=self.verbose_checkbox_var,
             justify="center",
-            text=render_text("إظهار النتائج"),
+            text=self.render_text("إظهار النتائج"),
             offvalue=0,
             onvalue=1,
         )
@@ -159,7 +162,7 @@ class App:
     def ask_for_input_path(self) -> None:
         input_path = filedialog.askopenfilename(
             initialdir="/",
-            title=render_text(constants.INPUT_DIALOG_TITLE),
+            title=self.render_text(constants.INPUT_DIALOG_TITLE),
             filetypes=(
                 ("Audio files", "*.mp3 *.wav *.m4a *.ogg"),
                 ("all files", "*.*"),
@@ -167,12 +170,11 @@ class App:
         )
         self.input_path.set(input_path)
 
-    @staticmethod
-    def on_error_occurs(error_msg: str) -> None:
-        messagebox.showerror("خطأ", error_msg)
+    def on_error_occurs(self, error_msg: str) -> None:
+        messagebox.showerror(self.render_text("خطأ"), error_msg)
 
     def open_win(self) -> None:
-        SettingWindow(self.parent, self.preference)
+        SettingWindow(self.parent, self.preference, self.render_text)
 
     def on_closing(self) -> None:
         self.parent.destroy()
@@ -180,7 +182,7 @@ class App:
 
     async def get_transcribe(self) -> None:
         if not self.preference.check_if_ar_key_exists():
-            self.on_error_occurs(render_text(constants.ERROR_API_KEY))
+            self.on_error_occurs(self.render_text(constants.ERROR_API_KEY))
 
         self.disable_entries()
         self.output_area.insert(tk.INSERT, "Please wait....\n")
@@ -210,3 +212,6 @@ class App:
 
     async def exec(self) -> None:
         await self.show()
+
+    def render_text(self, text: str) -> str:
+        return _text(self._platform, text)
