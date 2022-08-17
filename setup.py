59a1cb44613b5f0409e4e1cb9de418274f908141
yshalsager <ysh-alsager@hotmail.com>
2022-08-17 16:31:00 +0200

    chore(ci): Update cx_Freeze action
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- setup.py
+++ setup.py
@@ -2,23 +2,18 @@ import os
 import sys
 from pathlib import Path
 from shutil import copy
+from typing import List
 
 import cx_Freeze
 
 __version__ = "1.0.0"
 base = None
-include_files = ["assets/chat-centered-text-duotone.ico"]
+include_files: List[str] = []
 
 if sys.platform == "win32":
     base = "Win32GUI"
-    os.environ["TCL_LIBRARY"] = r"PATH_TO_PYTHON\\tcl\\tcl8.6"
-    os.environ["TK_LIBRARY"] = r"PATH_TO_PYTHON\\tcl\\tk8.6"
-    path = Path(__file__).parent.resolve()
-    if (path / "ffmpeg.exe").exists():
+    if (Path(__file__).parent / "ffmpeg.exe").exists():
         include_files.append("ffmpeg.exe")
-    build_path = path / "build" / "exe.win32-3.7"
-    copy(r"PATH_TO_PYTHON\\DLLs\\tcl86t.dll", build_path)
-    copy(r"PATH_TO_PYTHON\\DLLs\\tk86t.dll", build_path)
 
 includes = ["tkinter"]
 excludes = ["matplotlib", "sqlite3"]
@@ -39,8 +34,8 @@ packages = [
 ]
 
 cx_Freeze.setup(
-    name="Transcribe Arabic",
-    description="تحويل الملفات الصوتية الي نصوص",
+    name="TranscribeArabic",
+    description="تحويل الملفات الصوتية إلى نصوص",
     version=__version__,
     executables=[
         cx_Freeze.Executable(
@@ -62,7 +57,8 @@ cx_Freeze.setup(
         "bdist_msi": {
             "upgrade_code": "{00EF338F-794D-3AB8-8CD6-2B0AB7541021}",
             "add_to_path": False,
-            "initial_target_dir": rf"[ProgramFilesFolder]\TranscribeArabic",
+            "initial_target_dir": r"[ProgramFilesFolder]\TranscribeArabic",
+            "install_icon": "assets/chat-centered-text-duotone.ico",
         },
         "bdist_mac": {
             "iconfile": "chat-centered-text-duotone.icns",
