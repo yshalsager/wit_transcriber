cca4c97e352c691d172b3ba9895ded1309a67162
yshalsager <ysh-alsager@hotmail.com>
2022-08-17 14:49:59 +0200

    chore(packaging): Use cx_Freeze instead of Pyinstaller
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-import os
-import shutil
 import sys
+from pathlib import Path
+from typing import List
 
 import cx_Freeze
 
-os.environ["TCL_LIBRARY"] = r"PATH_TO_PYTHON\\tcl\\tcl8.6"
-os.environ["TK_LIBRARY"] = r"PATH_TO_PYTHON\\tcl\\tk8.6"
-
 __version__ = "1.0.0"
 base = None
+include_files: List[str] = []
+
 if sys.platform == "win32":
     base = "Win32GUI"
+    if (Path(__file__).parent / "ffmpeg.exe").exists():
+        include_files.append("ffmpeg.exe")
 
-include_files = ["ffmpeg.exe", "main.ico"]
 includes = ["tkinter"]
 excludes = ["matplotlib", "sqlite3"]
 packages = [
@@ -32,16 +32,16 @@ packages = [
 ]
 
 cx_Freeze.setup(
-    name="Transcribe Arabic",
-    description="تحويل الملفات الصوتية الي نصوص",
+    name="TranscribeArabic",
+    description="تحويل الملفات الصوتية إلى نصوص",
     version=__version__,
     executables=[
         cx_Freeze.Executable(
-            "wit_transcriber/gui/main_window.py",
+            "wit_transcriber/gui/app.py",
             base=base,
-            icon="main.ico",
-            shortcutName="Transcribe Arabic",
-            shortcutDir="DesktopFolder",
+            icon="assets/chat-centered-text-duotone.ico",
+            shortcut_name="Transcribe Arabic",
+            shortcut_dir="DesktopFolder",
         )
     ],
     options={
@@ -55,12 +55,16 @@ cx_Freeze.setup(
         "bdist_msi": {
             "upgrade_code": "{00EF338F-794D-3AB8-8CD6-2B0AB7541021}",
             "add_to_path": False,
-            "initial_target_dir": r"[ProgramFilesFolder]\%s" % ("TranscribeArabic"),
+            "initial_target_dir": r"[ProgramFilesFolder]\TranscribeArabic",
+            "install_icon": "assets/chat-centered-text-duotone.ico",
+        },
+        "bdist_mac": {
+            "iconfile": "assets/chat-centered-text-duotone.icns",
+            "bundle_name": "TranscribeArabic",
+        },
+        "bdist_dmg": {
+            "volume_label": "Install Transcribe Arabic",
+            "applications_shortcut": True,
         },
     },
 )
-
-path = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir))
-build_path = os.path.join(path, "build", "exe.win32-3.7")
-shutil.copy(r"PATH_TO_PYTHON\\DLLs\\tcl86t.dll", build_path)
-shutil.copy(r"PATH_TO_PYTHON\\DLLs\\tk86t.dll", build_path)
